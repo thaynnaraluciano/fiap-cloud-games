a4657a12dc2e0ec9c327fbb568fd26fe629ae995
antonioRenato <antoniorpfs@gmail.com>
2025-07-28 08:27:12 -0300

    Criado testes para promoções

---
+++ Tests/CommonTestUtilities/Commands/Promocoes/CriarPromocaoCommandBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using Domain.Commands.v1.Jogos.CriarJogo;
+using Domain.Commands.v1.Promocoes.CriarPromocao;
+
+namespace CommonTestUtilities.Commands.Promocoes
+{
+    public class CriarPromocaoCommandBuilder
+    {
+        public static CriarPromocaoCommand Build()
+        {
+            return new Faker<CriarPromocaoCommand>()
+            .RuleFor(c => c.Nome, f => f.Commerce.ProductName())
+            .RuleFor(c => c.Desconto, f => f.Random.Decimal(1, 100))
+            .RuleFor(c => c.DataInicio, f => f.Date.FutureOffset().DateTime)
+            .RuleFor(c => c.DataFim, (f, c) => c.DataInicio.AddDays(f.Random.Int(1, 10)))
+            .RuleFor(c => c.JogosIds, f => new List<Guid> { Guid.NewGuid() });
+        }
+    }
+}
---
+++ Tests/CommonTestUtilities/Commands/Promocoes/RemoverPromocaoCommandBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Commands.v1.Promocoes.RemoverPromocao;
+
+namespace CommonTestUtilities.Commands.Promocoes
+{
+    public class RemoverPromocaoCommandBuilder
+    {
+        public static RemoverPromocaoCommand Build()
+        {
+            return new RemoverPromocaoCommand(Guid.NewGuid());
+        }
+    }
+}
